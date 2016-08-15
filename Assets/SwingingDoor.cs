8af421fd9c9fbd5a18d869073a1e78e62fba9785
Joel Lee <lee.joel395@gmail.com>
2016-08-14 22:00:34 -0700

    Door is functional, planned fix for a camera bug.
    Door will now swing between two angles.
    
    Planned a fix for a camera issue where a picked up item is not added to
    the second camera to render over world objects.

--- Assets/SwingingDoor.cs
+++ Assets/SwingingDoor.cs
@@ -3,19 +3,30 @@ using System.Collections;
 
 public class SwingingDoor : MonoBehaviour {
     public Transform hinge;
-
+    public float swingspeed;
+    private float start = 0.0f;
+    private float finish = 90.0f;
+    private bool open = false;
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (open)
+        {
+            hinge.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, finish, 0), Time.deltaTime * swingspeed);
+        }
+        if(!open)
+        {
+           hinge.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, start, 0), Time.deltaTime * swingspeed);
+        }
+        
+    }
 
     private void activate(float signal)
     {
-        hinge.Rotate(0, 90, 0);
+        open = !open;
     }
 }
