573a63029c77ab56e64a2437bf42752f6cf45047
Joel Lee <lee.joel395@gmail.com>
2016-08-21 02:50:52 -0700

    Moving platform
    Moving platform goes between two points with button presses. Current
    issue is player does not stay on a platform during movement.

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+    public float velocity;
+
+    private Vector3 difference, destination;
+	// Use this for initialization
+	void Start () {
+        destination = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(destination);
+        if (transform.position != destination)
+        {
+            difference = destination - transform.position;
+            transform.position = Vector3.Lerp(transform.position, destination, Time.smoothDeltaTime);
+        }
+	}
+
+     public void ChangeDirection(Vector3 dest)
+    {
+        Debug.Log("Destination changed.");
+        destination = dest;
+    }
+
+    public Vector3 getPosition()
+    {
+        return transform.position;
+    }
+}
