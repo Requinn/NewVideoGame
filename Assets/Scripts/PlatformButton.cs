0e3b3393500a173de7465b792a1d4b0c4857fc36
Joel Lee <lee.joel395@gmail.com>
2016-09-16 12:22:24 -0700

    Fucking merge conflicts
    Fixed merge conflict with some metadata stuff. Ended up having to revert
    changes since github was refusing to comply with forced deletes and
    checking out.
    Removed some mountain assets I will probably re add later.
    
    To restate changes:
    Smoothed out moving platforms.
    Added a faceplayer script so things like enemy health bars will always
    be facing you.
    Added a UI element to track your ammo count.
    Added a little more things to the WIP level design.

---
+++ Assets/Scripts/PlatformButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformButton : MonoBehaviour {
+    public Transform pointA, pointB;
+    public MovingPlatform platform;
+    public bool atHome;
+	void Start () {
+        atHome = true;
+	}
+	//Platform will only move if it as either of the two points, so we can't just jiggle the plat in place.
+    void activate(float signal)
+    {
+        Debug.Log("ACTIVATED");
+        if (V3Equal(platform.getPosition(), pointB.position))
+        {
+            platform.ChangeDirection(pointA.position);
+        }
+        if (V3Equal(platform.getPosition(), pointA.position))
+        {
+            platform.ChangeDirection(pointB.position);
+        }
+    }
+
+    public bool V3Equal(Vector3 a, Vector3 b)
+    {
+        return Vector3.SqrMagnitude(a - b) < 0.001;
+    }
+
+}
